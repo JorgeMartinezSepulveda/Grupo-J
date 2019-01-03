711d52ca53ad6c4fcfd362f94cb309c62e0e4aee
JorgeMartinezSepulveda <actionjorge@gmail.com>
2019-01-03 19:55:19 +0100

    Añadido una primera aproximacion de WebSockets
    No es nada definitivo, una primera prueba de websockets con lo mas basico: mandar y recibir mensajes de manera bidireccional para ver que se comunican bien de una a otra sesion de juego

--- src/main/java/com/example/dagorDagorath/DagorDagorathApplication.java
+++ src/main/java/com/example/dagorDagorath/DagorDagorathApplication.java
@@ -2,11 +2,28 @@ package com.example.dagorDagorath;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
-public class DagorDagorathApplication {
+@EnableWebSocket
+public class DagorDagorathApplication implements WebSocketConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DagorDagorathApplication.class, args);
+	}
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) 
+	{
+		registry.addHandler(createOnlineHandler(), "/dagor").setAllowedOrigins("*");
+	}
+
+	@Bean
+	public OnlineHandler createOnlineHandler() 
+	{
+		return new OnlineHandler();
 	}
 }
